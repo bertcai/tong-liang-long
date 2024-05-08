b8378a23481ad0eabc02cda3f7583d2bb42bccc0
bert_cai <bert_cai@outlook.com>
2024-05-08 09:48:52 +0800

    add lottie animation and image page

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import './assets/main.css'
 import 'animate.css'
 
 import { createApp } from 'vue'
+import Vue3Lottie from 'vue3-lottie'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(Vue3Lottie)
 
 app.mount('#app')
--- src/views/science/SubImage.vue
+++ src/views/science/SubImage.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>图案纹样</div>
+  <Vue3Lottie class="fullscreen" :animationData="animationJson" :height="1080" :width="1920" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Vue3Lottie } from 'vue3-lottie'
+import animationJson from '@/assets/lottie/science/image.json'
+</script>
 
-<style scoped></style>
+<style scoped>
+.fullscreen {
+  position: fixed;
+  background: transparent;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+</style>
