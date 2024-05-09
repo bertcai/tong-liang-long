d68ec68a269f0f733709d7d04f151742247f56b2
bert_cai <bert_cai@outlook.com>
2024-05-09 22:38:28 +0800

    add leapmotion

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,9 @@
 </template>
 
 <script setup lang="ts">
+import { init, registerEventCallback } from 'touchfree/src'
+import type { TouchFreeInputAction } from 'touchfree/src'
 import { RouterLink, RouterView } from 'vue-router'
+
+init()
 </script>
