8de7e396d64331d506a391e67e794810be9ccf3f
bert_cai <bert_cai@outlook.com>
2024-05-09 10:02:20 +0800

    add audio

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { createApp } from 'vue'
 import Vue3Lottie from 'vue3-lottie'
 import App from './App.vue'
 import router from './router'
+import clickSound from './assets/audio/click.wav'
+import bgmSound from './assets/audio/bgm.flac'
 
 const app = createApp(App)
 
@@ -13,3 +15,15 @@ app.use(router)
 app.use(Vue3Lottie)
 
 app.mount('#app')
+
+// 添加全局的点击事件监听器
+document.addEventListener('click', () => {
+    const audio = new Audio(clickSound);
+    audio.play();
+});
+
+// 添加背景音乐
+const bgm = new Audio(bgmSound);
+bgm.loop = true;
+// bgm.volume = 0.1;
+bgm.play();
