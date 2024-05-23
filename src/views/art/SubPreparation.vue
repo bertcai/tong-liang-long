e5f9edd920753be311a605201a773bc6b819396c
bert_cai <bert_cai@outlook.com>
2024-05-23 16:58:22 +0800

    refactor: Update background image handling in SubPreparation.vue

--- src/views/art/SubPreparation.vue
+++ src/views/art/SubPreparation.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>准备工作</div>
+  <div class="wrapper"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount, ref, type Ref, inject } from 'vue'
+import preparation from '@/assets/img/art/preparation.gif'
 
-<style scoped></style>
+const app = document.querySelector('#app') as any
+const activeUrl = ref('')
+activeUrl.value = preparation + '?' + Math.random() || ''
+
+onMounted(() => {
+  activeUrl.value = preparation + '?' + Math.random() || ''
+  app.style.background = `url(${activeUrl.value}) no-repeat`
+})
+
+onBeforeUnmount(() => {
+  app.style.background = ''
+})
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: flex-start;
+  padding-top: 10px;
+  width: 100%;
+}
+</style>
